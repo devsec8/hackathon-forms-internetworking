05c5077b515a198fc088917023e03862f250720b
Or Abudi <orabudi@gmail.com>
2021-08-03 11:58:04 +0300

    added folders structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
