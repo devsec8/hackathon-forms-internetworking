ac4d99b8d8afc527fa387624949668b810b08557
danielco232 <danielco232@gmail.com>
2021-08-04 11:00:14 +0300

    add fanctionality to login component. WIP: make it work with the backend.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCard, MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { HomeComponent } from './components/home/home.component';
 import { FormComponent } from './components/form/form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import AuthService from './services/auth.service';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     LoginComponent,
     HomeComponent,
     FormComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -29,9 +32,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatButtonModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { filter, map, mergeMap, toArray } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Credentials } from '../interfaces/credentials.interface';
+
+@Injectable()
+export default class AuthService {
+  constructor(private http: HttpClient) { }
+
+  getCredentialsByUsername = (username: string): Observable<Credentials> => {
+    const api = 'https://c36f1db5-5fd9-4866-80c8-98182aa009de.mock.pstmn.io/form-internetworking/login?username=' + username;
+    return this.http.get<Credentials>
+      (api).pipe(map(res => {return {...res}}));
+  }
+}
